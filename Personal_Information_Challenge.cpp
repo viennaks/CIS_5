74506ed9c9a28559cbfa1133b3b9ad3dda6aa61b
Vi Le <53014202+viennaks@users.noreply.github.com>
2022-09-09 15:24:07 -0700

    Personal Information Challenge
    I had to write a program that displays the following pieces of information, each on a separate line.

---
+++ Personal_Information_Challenge.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Type your name here",
+           address = "Enter address",
+           phone = "Type phone number",
+           major = "Computer Science";
+
+    cout << endl;
+    cout << "Name: "          << name << endl;
+    cout << "Address: "       << address << endl;
+    cout << "Telephone #: "   << phone << endl;
+    cout << "College Major: " << major << endl;
+    cout << endl;
+
+    return 0;
+}
